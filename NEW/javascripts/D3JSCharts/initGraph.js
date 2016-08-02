edd3a375560fff935826e0910b6bab73d4023a2b
elie <titi38@github.com>
2016-08-02 17:44:11 +0200

    fin journée

--- NEW/javascripts/D3JSCharts/initGraph.js
+++ NEW/javascripts/D3JSCharts/initGraph.js
@@ -11,28 +11,24 @@ function emptyChartContainer(jqueryElement) {
 
 
 function drawChartFromInterface(urlJson, mydiv) {
+
     var div = d3.select(mydiv);
     if(div.size() === 0){
         return;
     }
 
-    var svg = div.select("svg");
-    if(svg.size() === 0){
-        return;
+
+    var divNode = div.node();
+    var divChild = divNode.firstChild;
+    while(divChild){
+
+        divNode.removeChild(divChild);
+        divChild = divNode.firstChild;
+
     }
 
-    if(svg.node().firstChild){
-        var divNode = div.node();
-        var divChild = divNode.firstChild;
-        while(divChild){
+    var svg = div.append("svg").classed("diagram",true);
 
-            divNode.removeChild(divChild);
-            divChild = divNode.firstChild;
-
-        }
-
-        svg = div.append("svg").classed("diagram",true);
-    }
     svg.classed("crisp",true);
     svg.margin = {top: 20, right: 50, bottom: 20, left: 60, zero:28};
 
@@ -50,24 +46,17 @@ function drawChart(urlJson, mydiv) {
     if(div.size() === 0){
         return;
     }
+    var divNode = div.node();
+    var divChild = divNode.firstChild;
+    while(divChild){
 
-    var svg = div.select("svg");
-    if(svg.size() === 0){
-        return;
+        divNode.removeChild(divChild);
+        divChild = divNode.firstChild;
+
     }
 
-    if(svg.node().firstChild){
-        var divNode = div.node();
-        var divChild = divNode.firstChild;
-        while(divChild){
+    var svg = div.append("svg").classed("diagram",true);
 
-            divNode.removeChild(divChild);
-            divChild = divNode.firstChild;
-
-        }
-
-        svg = div.append("svg").classed("diagram",true);
-    }
     svg.classed("crisp",true);
 
     svg.margin = {top: 50, right: 50, bottom: 50, left: 60, zero:28};

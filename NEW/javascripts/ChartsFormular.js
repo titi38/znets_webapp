a93601b2cb1d826ebffe44a47db159e5c772afe3
vladhack <ismael.zakari.toure@gmail.com>
2016-08-12 11:45:56 +0200

    patching chart form 'last' item select - part 2

--- NEW/javascripts/ChartsFormular.js
+++ NEW/javascripts/ChartsFormular.js
@@ -17,7 +17,7 @@ function updateChartsTimeslice(element ){
         case "lastWeek" :
         case "lastMonth" :
             $("#toDate_ChartsForm").data("DateTimePicker").date(moment('1900-01-01 00:01'));
-            $("#toDate_ChartsForm").data("DateTimePicker").date(moment().format('YYYY-MM-DD HH:mm'));
+            $("#toDate_ChartsForm").data("DateTimePicker").date(moment(serverDate).format('YYYY-MM-DD HH:mm'));
             $("#fromDate_ChartsForm").data("DateTimePicker").disable();
             $("#toDate_ChartsForm").data("DateTimePicker").disable();
             break;
@@ -46,8 +46,7 @@ function onChangeChartsForm() {
 
 function applyChartsForm(){
 
-    console.error($("#timestepCharts").val() === "MINUTE");
-    console.error($("#timestepCharts").val());
+    $("#timeslice_ChartsForm").val($("#timesliceCharts").val());
 
     $("#preset_ChartsForm").val($("#timestepCharts").val());
 

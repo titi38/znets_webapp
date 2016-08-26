7ed9372e437195c7a665e8737c016926ea6d5a1f
vladhack <ismael.zakari.toure@gmail.com>
2016-08-26 18:19:31 +0200

    adding mac resolution

--- javascripts/Localhosts.js
+++ javascripts/Localhosts.js
@@ -385,3 +385,81 @@ function addLocalhostTab(localhostIp, localhostName){
 }
 
 
+
+/**
+ * RawData Results DataTable - AS Name Retrieval Function
+ * THIS FUNCTION IS TRIGGERED ON ASNum TABLE CELL MOUSEOVER
+ * - Checks for cell's AS Name if it exists (Ajax request to server), once user's pointer stays over cell for 500ms
+ * - Ajax request result will trigger Cell's Tooltip definition
+ * @param el : cell element
+ */
+function retrieveASNum(el){
+
+    ///callAJAX("getAsList.json", '', "json", setASNumsId, null);
+    var element = $(el);
+    var delay = 500; // 0.5 seconds delay after last input
+
+    if($(element).attr('data-original-title') === "") {
+
+        clearTimeout(element.data('timer'));
+
+        if (element.html())
+            element.data('timer', setTimeout(function () {
+                element.removeData('timer');
+
+                // Do your stuff after 2 seconds of last user input
+                callAJAX("getAsName.json", 'as=' + element.html(), "json", setASToElementTitle, element);
+            }, delay));
+        else{
+            $(element).attr('data-original-title', "No AS");
+            $(element).attr("title", "No AS");
+        }
+
+    }
+
+}
+
+
+/**
+ * RawData Results DataTable - AS Name Retrieval Abortion Function
+ * THIS FUNCTION IS TRIGGERED ON ASNum TABLE CELL MOUSEOUT
+ * - Abort cell's AS Name Retrieval
+ * @param el : cell element
+ */
+function abordASNumRetrieval(el){
+
+    var element = $(el);
+
+    clearTimeout(element.data('timer'));
+
+}
+
+
+
+
+
+/**
+ * RawData Results DataTable - AS Cell's Tooltip Definition Function
+ * - Sets Cell's Tooltip definition
+ * - Trigger Cell Mouseover to show tooltip
+ * @param jsonResponse : server's response containing AS Name
+ * @param element : Cell Element
+ */
+function setASToElementTitle(jsonResponse, element){
+
+    var title = "AS Name not found";
+
+    if(jsonResponse)
+        if(jsonResponse.n)
+            title = jsonResponse.n;
+
+    element.parents("table").dataTable().find("div.asnumTooltip[data-toggle='tooltip'][value='"+element.attr("value")+"']").each(function() {
+        $(this).tooltip();
+        $(this).attr('data-original-title', title);
+        $(this).tooltip();
+    });
+
+    $(element).mouseover();
+
+}
+
